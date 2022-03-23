61f1877b76ef5600d6fe21150e88af35904d8bdb
SoftInterlude <juncheng.ge@outlook.com>
2022-03-23 17:27:13 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "show_bytes.cpp"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    test_show_bytes(20);
+    return 0;
+}
---
+++ show_bytes.cpp
@@ -0,0 +1,36 @@
+//
+// Created by 葛均诚 on 2022/3/23.
+//
+
+#include <cstdio>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len) {
+    size_t i;
+    for (i = 0; i < len; ++i) {
+        printf(" %.2x", start[i]);
+    }
+    printf("\n");
+}
+
+void show_int(int x) {
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x) {
+    show_bytes((byte_pointer)&x, sizeof (float ));
+}
+
+void show_pointer(void *x) {
+    show_bytes((byte_pointer)&x, sizeof (void *));
+}
+
+void test_show_bytes(int val) {
+    int ival = val;
+    float fval = (float) ival;
+    int *pval = &ival;
+    show_int(ival);
+    show_float(fval);
+    show_pointer(pval);
+}
